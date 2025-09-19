10a821363c17650e38e21b1483113e62bdc0cf8a
Gary Froyland <g.froyland@unsw.edu.au>
2025-09-19 16:45:50 +1000

    Illustrative demo

--- julia/demo.jl
+++ julia/demo.jl
@@ -2,12 +2,15 @@ using Plots
 
 include("SEBA.jl")
 
+# create a data array V with 4 vectors
 grid = 0:0.1:2π
 numvecs = 4
 V = [cos.((k / 2) * x) for x ∈ grid, k = 0:numvecs-1]
 
+# apply SEBA to obtain sparse array S with similar column space to V
 S = SEBA(V)
 
+# plot initial data and SEBA result
 data_plot = plot(grid, V, legend=false, title="$numvecs original vectors")
 SEBA_plot = plot(grid, S, legend=false, title="$numvecs sparse SEBA vectors with similar span")
 
